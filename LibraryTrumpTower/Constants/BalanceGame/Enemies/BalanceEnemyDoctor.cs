1fa42485ef22eccfe607c90689df7a3f32132d60
Dumontier <jolann.dumontier.pro@gmail.com>
2018-01-31 02:29:48 +0100

    Little nerf to Doc's heal

--- LibraryTrumpTower/Constants/BalanceGame/Enemies/BalanceEnemyDoctor.cs
+++ LibraryTrumpTower/Constants/BalanceGame/Enemies/BalanceEnemyDoctor.cs
@@ -12,7 +12,7 @@ namespace LibraryTrumpTower.Constants.BalanceGame.Enemies
         public static double ENEMY_DOCTOR_DAMAGE = 100;
         public static double ENEMY_DOCTOR_DEFAULT_SPEED = 3;
         public static int ENEMY_DOCTOR_BOUNTY = 80;
-        public static double ENEMY_DOCTOR_HEAL = 50;
+        public static double ENEMY_DOCTOR_HEAL = 40;
         public static double ENEMY_DOCTOR_HEAL_COOLDOWN = 3;
         public static double ENEMY_DOCTOR_ACTION_RADIUS = 2.2;
     }
